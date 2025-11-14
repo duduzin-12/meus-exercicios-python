fbde5b5e6cfcdf31c42c57e354973cab673c82e9
duduzin-12 <eduardoolinto4@gmail.com>
2025-11-14 15:31:15 -0300

    Algumas atualizações no front-end

--- projeto-api-flask/api.py
+++ projeto-api-flask/api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,6 +15,30 @@ turma = [
 
 def pegar_alunos():
   return jsonify(turma)
+
+@app.route("/api/aluno", methods=['POST'])
+def adicionar_aluno():
+
+    dados = request.json
+    
+    turma.append(dados)
+    
+    print(f"LOG: Novo aluno adicionado: {dados}")
+    
+    return jsonify({"status": "sucesso", "aluno_adicionado": dados})
+
+@app.route("/api/aluno/<string:nome>", methods=['DELETE'])
+def deletar_aluno(nome):
+
+    global turma
+
+    turma_atualizada = [aluno for aluno in turma if aluno['nome'] != nome]
+
+    turma = turma_atualizada
+
+    print(f"LOG: Aluno {nome} deletado.")
+
+    return jsonify({"status": "sucesso", "aluno_deletado": nome})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- projeto-api-flask/script.js
+++ projeto-api-flask/script.js
@@ -1,10 +1,61 @@
-fetch('http://127.0.0.1:5000/api/alunos')
-  .then(resposta => resposta.json())
-  .then(dados_da_turma =>{
-    const listaElementos = document.getElementById("lista-de-alunos")
-    for (let aluno of dados_da_turma){
-      const item = document.createElement("li")
-      item.textContent = `Nome: ${aluno.nome}, Nota: ${aluno.nota}`
-      listaElementos.appendChild(item)
-    }
-  })
+const formNovoAluno = document.getElementById("form-novo-aluno");
+const inputNome = document.getElementById("nome-aluno");
+const inputNota = document.getElementById("nota-aluno");
+const listaElementos = document.getElementById("lista-de-alunos");
+
+function buscarAlunos(){
+  fetch('http://127.0.0.1:5000/api/alunos')
+  .then(resposta => resposta.json())
+  .then(dados_da_turma =>{
+    listaElementos.innerHTML = "";
+    for (let aluno of dados_da_turma){
+      const item = document.createElement("li")
+      item.textContent = `Nome: ${aluno.nome}, Nota: ${aluno.nota}`
+      item.dataset.nome = aluno.nome;
+      listaElementos.appendChild(item)
+    }
+  })
+};
+
+listaElementos.addEventListener("click", (event) => {
+  if(event.target.tagName === 'LI'){
+    const nomeParaDeletar = event.target.dataset.nome;
+    event.target.remove(); 
+    fetch(`http://127.0.0.1:5000/api/aluno/${nomeParaDeletar}`, {
+      method: 'DELETE',
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+      console.log("Resposta do servidor:", dados);
+    });
+  };
+});
+
+formNovoAluno.addEventListener("submit", (event) =>{
+  event.preventDefault();
+
+  const nome = inputNome.value;
+  const nota = inputNota.value;
+
+  const novoAluno = {
+        nome: nome,
+        nota: parseFloat(nota)
+  };
+
+  fetch('http://127.0.0.1:5000/api/aluno', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+        body: JSON.stringify(novoAluno),
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+        console.log("Resposta do servidor:", dados);
+
+        inputNome.value = "";
+        inputNota.value = "";
+
+        buscarAlunos();
+    });
+});
