f6416e6058ef55c07cdc4c064f8f23fe756c63a0
duduzin-12 <eduardoolinto4@gmail.com>
2025-11-14 13:02:21 -0300

    Refatorando: Organizando arquivos em pastas

---
+++ projeto-api-flask/api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+turma = [
+  {"nome": "Ana", "nota": 8.5,},
+  {"nome": "Bruno", "nota": 4.1,},
+  {"nome": "Julia", "nota": 9,},
+  {"nome": "Eduardo", "nota": 10,}
+]
+
+@app.route("/api/alunos")
+
+def pegar_alunos():
+  return jsonify(turma)
+
+if __name__ == '__main__':
+    app.run(debug=True)
